290d325b91eddecbbcb90796b3a44261481c9226
Lucas-Haux <hauxlucas2005@gmail.com>
2024-06-02 14:40:26 -0700

    change wallpaper

--- homeManager/modules/hyprpaper/default.nix
+++ homeManager/modules/hyprpaper/default.nix
@@ -10,11 +10,11 @@
       splash = false;
       splash_offset = 2.0;
 
-      preload = ["/home/luke/Pictures/nixWallpaper.png"];
+      preload = ["/home/luke/Pictures/rustWallpaper.png"];
 
       wallpaper = [
-        "HDMI-A-1,/home/luke/Pictures/nixWallpaper.png"
-        "DP-2,/home/luke/Pictures/nixWallpaper.png"
+        "HDMI-A-1,/home/luke/Pictures/rustWallpaper.png"
+        "DP-2,/home/luke/Pictures/rustWallpaper.png"
       ];
     };
   };
