9bc9f6046e44d130551c13748bb8df049e2376b8
Lucas-Haux <hauxlucas2005@gmail.com>
2024-07-03 11:41:06 -0700

    static ip

--- nixosConfig/modules/serverNetwork/default.nix
+++ nixosConfig/modules/serverNetwork/default.nix
@@ -11,4 +11,52 @@
     '';
     wantedBy = [ "basic.target" ]; # starts after login
   };
+  networking = {
+  interfaces.eno1 = {
+    ipv6.addresses = [{
+      address = "2601:603:4c7e:3e40:82a2:6bdb:9c54:6db0";
+      prefixLength = 64;
+    }];
+    ipv4.addresses = [{
+      address = "10.0.0.178";
+      prefixLength = 24;
+    }];
+  };
+  defaultGateway = {
+    address = "10.0.0.1";
+    interface = "eno1";
+  };
+  defaultGateway6 = {
+    address = "fe80::1";
+    interface = "eno1";
+  };
+#
+#   nftables = {
+#     enable = true;
+#     ruleset = ''
+#         table ip nat {
+#           chain PREROUTING {
+#             type nat hook prerouting priority dstnat; policy accept;
+#             iifname "eno1" tcp dport 65534 dnat to 10.0.0.178:65534
+#           }
+#         }
+#     '';
+#   };
+#   firewall = {
+#     enable = true;
+#     allowedTCPPorts = [ 65534 ];
+#   };
+#   nat = {
+#     enable = true;
+#     internalInterfaces = [ "en01" ];
+#     externalInterface = "wg0";
+#     forwardPorts = [
+#       {
+#         sourcePort = 65534;
+#         proto = "tcp";
+#         destination = "10.0.0.178:65534";
+#       }
+#     ];
+#   };
+ };
 }
