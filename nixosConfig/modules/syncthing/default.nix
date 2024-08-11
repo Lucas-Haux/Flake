8d72022a336a4bb2f7fa062cc4aa224f6ae85a9b
Lucas-Haux <hauxlucas2005@gmail.com>
2024-08-11 03:18:21 -0700

    Bug: fix syncthing not saving settings on restart

--- nixosConfig/modules/syncthing/default.nix
+++ nixosConfig/modules/syncthing/default.nix
@@ -2,6 +2,5 @@
   environment.systemPackages = [ pkgs.syncthing ]; #shouldnt be needed but I think it helped with my config saving
   services.syncthing = {
     enable = true;
-    options.urAccepted = -1;
   };
 }
