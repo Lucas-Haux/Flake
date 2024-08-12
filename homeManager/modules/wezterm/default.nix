41f13f81fbd876872226fbc7205cd83ea2e18e42
Lucas-Haux <hauxlucas2005@gmail.com>
2024-08-11 21:23:27 -0700

    Config: minor config changes to wezterm

--- homeManager/modules/wezterm/default.nix
+++ homeManager/modules/wezterm/default.nix
@@ -1,19 +1,26 @@
-{
-  pkgs,
-  config,
-  lib,
-  ...
-}: {
+{ pkgs, config, lib, ... }: {
+  home.sessionVariables.TERMINAL = "wezterm";
   programs.wezterm = {
     enable = true;
     enableZshIntegration = true;
-    extraConfig = "
+    extraConfig = ''
       return {
         enable_wayland = false,
-	color_scheme = 'nord',
-	hide_tab_bar_if_only_one_tab = true,
-	window_background_opacity = 0.7
+
+        color_scheme = 'nordfox',
+        window_background_opacity = 0.7,
+
+        hide_tab_bar_if_only_one_tab = true,
+        use_fancy_tab_bar            = false,
+
+        audible_bell = disabled,
+        window_padding = {
+          left = 3, right = 3,
+          top = 3, bottom = 3,
+        },
+        allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace",
+        adjust_window_size_when_changing_font_size = false,
       }
-    ";
+    '';
   };
 }
