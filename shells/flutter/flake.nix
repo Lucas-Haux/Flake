322a5e45fb4a8126b2de9366c8d83e4af9108ff0
Lucas-Haux <hauxlucas2005@gmail.com>
2024-09-25 18:17:26 -0700

    Shells: Add flutter development nix shell

--- shells/flutter/flake.nix
+++ shells/flutter/flake.nix
@@ -1,12 +1,15 @@
 {
-description = "Flutter 3.13.x";
-inputs = {
-  nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  flake-utils.url = "github:numtide/flake-utils";
-};
-outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem (system:
-    let
+  description = "Flutter 3.13.x";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
         config = {
@@ -16,21 +19,20 @@ outputs = { self, nixpkgs, flake-utils }:
       };
       buildToolsVersion = "34.0.0";
       androidComposition = pkgs.androidenv.composeAndroidPackages {
-        buildToolsVersions = [ buildToolsVersion "30.0.3" ];
-        platformVersions = [ "34" "28" ];
-        abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
+        buildToolsVersions = [buildToolsVersion "30.0.3"];
+        platformVersions = ["34" "28"];
+        abiVersions = ["armeabi-v7a" "arm64-v8a"];
       };
       androidSdk = androidComposition.androidsdk;
-    in
-    {
-      devShell =
-        with pkgs; mkShell rec {
+    in {
+      devShell = with pkgs;
+        mkShell rec {
           ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
           buildInputs = [
             # flutterPackages-source.stable
             # flutterPackages-source.v3_22
-            flutter 
-            androidSdk # The customized SDK that we've made above
+            flutter
+            #androidSdk # The customized SDK that we've made above
             jdk17
             android-studio
           ];
