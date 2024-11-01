16327559eea5954da2a46e669c2049217d06e01c
Lucas-Haux <hauxlucas2005@gmail.com>
2024-11-01 14:53:12 -0700

    Update Flake, add unstable channel

--- flake.nix
+++ flake.nix
@@ -1,10 +1,11 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.4.1";
