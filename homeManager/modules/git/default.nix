c84efa957253367860544a396d80fc28cad95a04
Lucas-Haux <hauxlucas2005@gmail.com>
2024-12-23 16:31:02 -0800

    Make github default branch name main

--- homeManager/modules/git/default.nix
+++ homeManager/modules/git/default.nix
@@ -1,8 +1,11 @@
-{ pkgs, config, ... }:
+{ ... }:
 {
   programs.git = {
     enable = true;
     userName = "Lucas-Haux";
     userEmail = "developer@lucas-haux.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
   };
 }
