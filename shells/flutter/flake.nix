5ae8572c6f44d278fd86ae4f3b9053e56b5b4b9e
Lucas-Haux <hauxlucas2005@gmail.com>
2025-01-20 18:44:07 -0800

    Flutter Shell: Switch to stabel packages, add more build tools and platfor versions to android composition, add shell variables, switch to flutter 324, add sdk8 support, and more code comments

--- shells/flutter/flake.nix
+++ shells/flutter/flake.nix
@@ -1,70 +1,103 @@
 {
-  description = "Flutter 3.13.x";
+  description = "Flutter environment";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs";
   };
+
   outputs =
     {
       self,
       nixpkgs,
       flake-utils,
     }:
-    flake-utils.lib.eachDefaultSystem (
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
       system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          config = {
-            android_sdk.accept_license = true;
-            allowUnfree = true;
-          };
+          android_sdk.accept_license = true;
+          allowUnfree = true;
+          config.permittedInsecurePackages = [
+            "olm-3.2.16"
+          ];
         };
-        buildToolsVersion = "34.0.0";
-        androidComposition = pkgs.androidenv.composeAndroidPackages {
+        androidEnv = pkgs.androidenv.override { licenseAccepted = true; };
+        androidComposition = androidEnv.composeAndroidPackages {
+          ## Details about default values: https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/mobile/androidenv/compose-android-packages.nix
+          #
           buildToolsVersions = [
-            buildToolsVersion
             "30.0.3"
+            "35.0.0"
           ];
           platformVersions = [
+            "31"
+            "32"
+            "33"
             "34"
-            "28"
-          ]; # "VanillaIceCream"
-          abiVersions = [
-            "armeabi-v7a"
-            "arm64-v8a"
           ];
-          # includeEmulator = false;
-          # emulatorVersion = "35.1.4";
-          # includeSystemImages = false;
-          # systemImageTypes = ["google_apis_playstore"];
+          cmakeVersions = [ "3.18.1" ];
+          includeNDK = true;
+          ndkVersion = "23.1.7779620";
+          useGoogleAPIs = true;
+          extraLicenses = [
+            "android-googletv-license"
+            "android-sdk-arm-dbt-license"
+            "android-sdk-license"
+            "android-sdk-preview-license"
+            "google-gdk-license"
+            "intel-android-extra-license"
+            "intel-android-sysimage-license"
+            "mips-android-sysimage-license"
+          ];
         };
         androidSdk = androidComposition.androidsdk;
       in
       {
         devShell =
           with pkgs;
-          mkShell {
-            ANDROID_SDK_ROOT = "/home/luke/Projects/flutter/Android/Sdk/platforms/android-35/google_apis_playstore";
-
+          mkShell rec {
+            ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
+            ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+            JAVA_HOME = jdk17.home;
+            JAVA_8_HOME = jdk8.home;
+            JAVA_17_HOME = jdk17.home;
+            FLUTTER_ROOT = flutter324;
+            DART_ROOT = "${flutter324}/bin/cache/dart-sdk";
+            GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/35.0.0/aapt2";
+            QT_QPA_PLATFORM = "wayland;xcb"; # emulator related: try using wayland, otherwise fall back to X
+            # NB: due to the emulator's bundled qt version, it currently does not start with QT_QPA_PLATFORM="wayland".
+            # Maybe one day this will be supported.
             buildInputs = [
-              # flutterPackages-source.stable
-              # flutterPackages-source.v3_24
-              flutter
-              firebase-tools
-              androidSdk # The customized SDK
-              jdk17
+              ## General needs
+              flutter324
 
-              android-tools
-              android-studio
-              # android-studio-full
-              # xcode-install
-              nodejs_22
-              nodePackages.eslint
-              nodePackages.firebase-tools
+              ## Android target
+              androidSdk
+              jdk17
+              jdk8 # for gradle sake...
             ];
+            # Globally installed packages, which are installed through `dart pub global activate package_name`,
+            # are located in the `$PUB_CACHE/bin` directory.
+            shellHook = ''
+              if [ -z "$PUB_CACHE" ]; then
+                export PATH="$PATH:$HOME/.pub-cache/bin"
+              else
+                export PATH="$PATH:$PUB_CACHE/bin"
+              fi
+
+              flutter doctor -v
+
+              ## Just to please Gradle with a JDK8
+              echo
+              echo
+              echo "Please Make Sure That: '$HOME/.gradle/gradle.properties' contains:"
+              echo "org.gradle.java.installations.paths=$JAVA_8_HOME,$JAVA_17_HOME"
+              echo
+              echo
+            '';
           };
-        builtins.exec = [ "zsh" ];
       }
     );
 }
