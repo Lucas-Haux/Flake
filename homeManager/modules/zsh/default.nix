9eab03d4eab4f949c964c942bfab5e580bcf5241
Lucas-Haux <hauxlucas2005@gmail.com>
2025-01-26 20:17:05 -0800

    Feature: Add zsh oh-my-zsh plugins and aliases

--- homeManager/modules/zsh/default.nix
+++ homeManager/modules/zsh/default.nix
@@ -2,8 +2,24 @@
 {
   programs.zsh = {
     enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+    enableAutosuggestions = true;
+    autocd = true;
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "vi-mode"
+        "colorize"
+        "colored-man-pages"
+        "pj"
+      ];
+    };
+
     initExtra = # bash
       ''
+        export KEYTIMEOUT=1
+
         function y() {
         	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
         	yazi "$@" --cwd-file="$tmp"
@@ -14,10 +30,23 @@
         };
 
         nixsearch() {
-          nix search nixpkgs "$@" 2>/dev/null | sed -E 's/legacyPackages\.[a-z0-9_\-]+\.//'
+          nix search "$@" 2>/dev/null | sed -E 's/legacyPackages\.[a-z0-9_\-]+\.//'
         }
 
-         export EDITOR="nvim"
+        # for pj plugin
+        PROJECT_PATHS=(~/Projects/flutter)
+
+        export EDITOR="nvim"
       '';
+
+    shellAliases = {
+      ll = "ls -l";
+      rebuild = "sudo nixos-rebuild switch --flake ~/Flake#desktop";
+      home = "home-manager switch --flake ~/Flake/.#luke@desktop";
+      flutter-shell = "export NIXPKGS_ALLOW_UNFREE=1; nix develop --impure ~/Flake/shells/flutter -c zsh";
+      webcamOn = "adb -s 19271FDEE00013 shell svc usb setFunctions uvc";
+      webcamOff = "adb -s 19271FDEE00013 usb";
+    };
+
   };
 }
