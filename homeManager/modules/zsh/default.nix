a7cd1eab70eef3f2df3aae70c178ef68b4b91c35
Lucas-Haux <developer@lucas-haux.com>
2025-03-15 17:11:38 -0700

    Zsh Features: Add nixShell Function and add aliases for rebuilding home-manager and nixos for the server

--- homeManager/modules/zsh/default.nix
+++ homeManager/modules/zsh/default.nix
@@ -1,10 +1,10 @@
-{ pkgs, config, ... }:
+{ ... }:
 {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    syntaxHighlighting.enable = true;
     enableAutosuggestions = true;
+    syntaxHighlighting.enable = true;
     autocd = true;
     oh-my-zsh = {
       enable = true;
@@ -29,8 +29,12 @@
         	rm -f -- "$tmp"
         };
 
-        nixsearch() {
+        nixSearch() {
           nix search "$@" 2>/dev/null | sed -E 's/legacyPackages\.[a-z0-9_\-]+\.//'
+        }
+
+        nixShell() {
+          nix-shell -p "$@" --run zsh
         }
 
         # for pj plugin
@@ -41,9 +45,14 @@
 
     shellAliases = {
       ll = "ls -l";
+
       rebuild = "sudo nixos-rebuild switch --flake ~/Flake#desktop";
+      rebuild-server = "sudo nixos-rebuild switch --flake ~/Flake#server";
       home = "home-manager switch --flake ~/Flake/.#luke@desktop";
+      home-server = "home-manager switch --flake ~/Flake/.#luke@server";
+
       flutter-shell = "export NIXPKGS_ALLOW_UNFREE=1; nix develop --impure ~/Flake/shells/flutter -c zsh";
+
       webcamOn = "adb -s 19271FDEE00013 shell svc usb setFunctions uvc";
       webcamOff = "adb -s 19271FDEE00013 usb";
     };
