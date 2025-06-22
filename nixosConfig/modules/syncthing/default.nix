39b95bbd8c646c4d08bfa8ac99592b9a6d00354c
Lucas-Haux <developer@lucas-haux.com>
2025-06-22 15:06:02 -0700

    Syncthing: Clean up unused code

--- nixosConfig/modules/syncthing/default.nix
+++ nixosConfig/modules/syncthing/default.nix
@@ -1,12 +1,5 @@
+{ ... }:
 {
-  config,
-  pkgs,
-  lib,
-  ...
-}:
-with lib;
-{
-  environment.systemPackages = [ pkgs.syncthing ]; # shouldnt be needed but I think it helped with my config saving
   services.syncthing = {
     enable = true;
     user = "luke";
