b286095190bdc196cb568d551b5987543d6cd2d8
Lucas-Haux <developer@lucas-haux.com>
2025-06-22 20:01:47 -0700

    Zsh: Rename commands

--- homeManager/modules/zsh/default.nix
+++ homeManager/modules/zsh/default.nix
@@ -20,15 +20,18 @@
       ''
         export KEYTIMEOUT=1
 
-        nix-search() {
-          nix search "$@" 2>/dev/null | sed -E 's/legacyPackages\.[a-z0-9_\-]+\.//'
-        }
+        # BROKEN
+        # search for packages within the shell
+        # nixSearch() {
+        #   nix-search "$@" 2>/dev/null | sed -E 's/legacyPackages\.[a-z0-9_\-]+\.//'
+        # }
 
-        nix-shell() {
+        # Quick Shell
+        qs() {
           nix-shell -p "$@" --run zsh
         }
 
-        # for pj plugin
+        # For pj(Project Jump) Plugin
         PROJECT_PATHS=(~/Projects/flutter)
 
         export EDITOR="nvim"
@@ -38,13 +41,16 @@
       ll = "ls -l";
       cd = "z"; # replace cd with zoxide
 
+      # Nixos And Home Manager Switch Aliases
       rebuild = "sudo nixos-rebuild switch --flake ~/Flake#desktop";
       rebuild-server = "sudo nixos-rebuild switch --flake ~/Flake#server";
       home = "home-manager switch --flake ~/Flake/.#luke@desktop";
       home-server = "home-manager switch --flake ~/Flake/.#luke@server";
 
+      # Quickly Enter My Flutter Dev Shell
       flutter-shell = "export NIXPKGS_ALLOW_UNFREE=1; nix develop --impure ~/Flake/shells/flutter -c zsh";
 
+      # Toggle My Phone Webcam
       webcam-on = "adb -s 19271FDEE00013 shell svc usb setFunctions uvc";
       webcam-off = "adb -s 19271FDEE00013 usb";
     };
