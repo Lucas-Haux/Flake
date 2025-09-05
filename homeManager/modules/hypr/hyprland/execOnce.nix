8b1476dddbf18eb8d1e5c92bba78ce850975cdb5
Lucas-Haux <developer@lucas-haux.com>
2025-09-04 22:51:42 -0700

    Hyprland: Make hyprpanel and zen-beta autostart

--- homeManager/modules/hypr/hyprland/execOnce.nix
+++ homeManager/modules/hypr/hyprland/execOnce.nix
@@ -8,12 +8,13 @@
   "bash $HOME/Flake/homeManager/modules/hypr/scripts/btop_close_script.bash" # used for hyprpanel btop popup
 
   # Programs
+  "hyprpanel"
   "hyprpaper"
   "Discord"
   "android-messages"
   "obsidian"
   "spotify"
   "chromium --app=https://t3.chat --user-data-dir=$HOME/.config/webapps/t3_chat" # used for ai special workspace
-  "zen"
+  "zen-beta"
   # "swaync-client -df"
 ]
