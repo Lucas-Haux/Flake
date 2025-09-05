36aade4f3a55fb65dd93a8e2a236136d8ff2980f
Lucas-Haux <developer@lucas-haux.com>
2025-09-04 22:52:18 -0700

    Hyprland: Major window rule config changes

--- homeManager/modules/hypr/hyprland/rules.nix
+++ homeManager/modules/hypr/hyprland/rules.nix
@@ -11,6 +11,12 @@
       "7, monitor:HDMI-A-1, default:true"
       "8, monitor:HDMI-A-1"
       "9, monitor:HDMI-A-1"
+      "special:AI, gapsout:75"
+      "special:discord, gapsout:75"
+      "special:spotify, gapsout:75"
+      "special:android-messages-desktop, gapsout:75"
+      "special:ProtonPass, gapsout:75"
+
     ];
 
     windowrule = [
@@ -28,23 +34,34 @@
       "workspace special:discord silent,class:(discord)"
       "opacity 0.8 0.8, class:(discord)"
       "noscreenshare, class:(discord)"
+      "bordercolor rgba(${lib.removePrefix "#" config.colorscheme.colors.base0E}cc), class:(discord)"
 
       # Obsidian special
       "tile,class:(obsidian)"
       "workspace special:obsidian silent,class:(obsidian)"
       "opacity 0.8 0.8, class:(obsidian)"
-      "noscreenshare, class:(obsidian)"
+      # "noscreenshare, class:(obsidian)"
+      "bordercolor rgba(${lib.removePrefix "#" config.colorscheme.colors.base0E}cc) rgba(${lib.removePrefix "#" config.colorscheme.colors.base03}cc), class:(obsidian)"
 
       # Android Message Special
       "tile,class:(android-messages-desktop)"
       "workspace special:android-messages-desktop silent,class:(android-messages-desktop)"
       "opacity 0.8 0.8, class:(android-messages-desktop)"
       "noscreenshare, class:(android-messages-desktop)"
+      "bordercolor rgba(${lib.removePrefix "#" config.colorscheme.colors.base0E}cc), class:(android-messages-desktop)"
 
       # Spotify Special
       "tile,class:(spotify)"
       "workspace special:spotify silent,class:(spotify)"
       "opacity 0.75 0.75, class:(spotify)"
+      "bordercolor rgba(${lib.removePrefix "#" config.colorscheme.colors.base0E}cc), class:(spotify)"
+
+      # Proton Pass Special
+      "tile,class:(Proton Pass)"
+      "workspace special:ProtonPass silent,class:(Proton Pass)"
+      "opacity 0.75 0.75, class:(Proton Pass)"
+      "bordercolor rgba(${lib.removePrefix "#" config.colorscheme.colors.base0E}cc), class:(Proton Pass)"
+      "noscreenshare, class:(Proton Pass)"
 
       # Clipse
       "float, class:(popup.clipse)"
@@ -62,11 +79,12 @@
       "content video, title:^(Picture-in-Picture)"
       "renderunfocused, title:^(Picture-in-Picture)"
 
-      # t3.chat
+      # t3.chat Special
       "workspace special:AI silent,class:(chrome-t3.chat__-Default)"
-      "float, class:(chrome-t3.chat__-Default)"
-      "center=1, class:(chrome-t3.chat__-Default)"
-      "size 65% 90%, class:(chrome-t3.chat__-Default)"
+      # "float, class:(chrome-t3.chat__-Default)"
+      "tile, class:(chrome-t3.chat__-Default)"
+      # "center=1, class:(chrome-t3.chat__-Default)"
+      # "size 65% 90%, class:(chrome-t3.chat__-Default)"
       "opacity 0.75 0.75, class:(chrome-t3.chat__-Default)"
       # "stayfocused, class:(chrome-t3.chat__-Default)"
       "bordercolor rgba(${lib.removePrefix "#" config.colorscheme.colors.base0E}cc), class:(chrome-t3.chat__-Default)"
