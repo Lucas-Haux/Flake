72d38036379f82d98f0dc8231dd08c98de17e971
Lucas-Haux <developer@lucas-haux.com>
2026-06-16 09:41:51 -0700

    Update

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
+      url = "github:0xc000022070/zen-browser-flake/beta";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     spicetify-nix = {
