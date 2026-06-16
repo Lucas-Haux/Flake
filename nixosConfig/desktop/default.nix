1c3b6537fb4c2d43fe82f5d9b392106d7b5ba869
Lucas-Haux <developer@lucas-haux.com>
2026-06-16 09:41:03 -0700

    Boot loader Changes

--- nixosConfig/desktop/default.nix
+++ nixosConfig/desktop/default.nix
@@ -20,9 +20,30 @@
 
   # Bootloader.
   boot.loader = {
-    systemd-boot.enable = true;
+    # systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
     timeout = 60;
+    grub = {
+      # despite what the configuration.nix manpage seems to indicate,
+      # as of release 17.09, setting device to "nodev" will still call
+      # `grub-install` if efiSupport is true
+      # (the devices list is not used by the EFI grub install,
+      # but must be set to some value in order to pass an assert in grub.nix)
+      devices = [ "nodev" ];
+      efiSupport = true;
+      enable = true;
+      # set $FS_UUID to the UUID of the EFI partition
+      extraEntries = ''
+        menuentry "Windows" {
+          insmod part_gpt
+          insmod fat
+          insmod search_fs_uuid
+          insmod chain
+          search --fs-uuid --set=root $FS_UUID
+          chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+        }
+      '';
+    };
   };
 
   # Security
